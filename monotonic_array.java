0a4e40befbb801e06588ba27ebc980d9f50b4f37
Rachel Owens <rachel.e.owens@gmail.com>
2023-10-01 17:35:22 -0500

    fixed monotonic_array

--- monotonic_array.java
+++ monotonic_array.java
@@ -8,14 +8,14 @@ class Solution {
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > nums[i - 1]) { // increasing
-                if (direction = 0)
+                if (direction == 0)
                     direction = 1;
-                else if (direction = -1)
+                else if (direction == -1)
                     return false;
             } else if (nums[i] < nums[i - 1]) { // decreasing
-                if (direction = 0)
+                if (direction == 0)
                     direction = -1;
-                else if (direction = 1)
+                else if (direction == 1)
                     return false;
             }
         }
